0146a47b2e1f59d1dcb54b0773655d9bbaecff3e
shinji-ono94 <1530sinnzi1003@gmail.com>
2022-06-23 23:57:36 +0900

    NEW

---
+++ README.md
@@ -0,0 +1,4 @@
+## MPMD
+
+# Reference
+- [Javaの並列化コードサンプル集](https://qiita.com/thtitech/items/3b874fe589b61239506f)
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class App{
+
+    public static void main(String[] args) {
+        final List<String> query1List = new ArrayList<String>();
+        final List<String> res1 = new ArrayList<String>();
+        final List<String> query2List = new ArrayList<String>();
+        final List<String> res2 = new ArrayList<String>();
+        for(int i = 0; i < 10; i++){
+            query1List.add(Integer.toString(i));
+            query2List.add(Integer.toString(i+10));
+        }
+        Thread t1 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                query1List.stream().forEach(x -> {res1.add(x + "a");});
+            }
+        });
+        Thread t2 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                query2List.stream().forEach(x ->{res2.add(x + "b");});
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
